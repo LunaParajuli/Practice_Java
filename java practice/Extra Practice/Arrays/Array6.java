9ecd41a85c4417c79b47090118a3ab129d399d3e
LunaParajuli <parajuliluna8@gmail.com>
2024-01-21 21:52:13 +0545

    no comment

---
+++ java practice/Extra Practice/Arrays/Array6.java
@@ -0,0 +1,16 @@
+
+class Employee
+{
+  int empID = 2;
+  int basicSal = 20000;
+  int incentive = 200; 
+}
+
+public class Array6
+{
+  public static void main(String args[])
+  {
+   Employee emp = new Employee();
+   System.out.println(emp.empID);
+  }
+}
